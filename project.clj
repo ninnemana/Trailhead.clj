1ee8cb97020f0b1d01cdc44e8c4fd2a458c2638f
clojure <clojure@silverwalla.(none)>
2011-12-01 23:14:07 -0600

    finished writing html

--- project.clj
+++ project.clj
@@ -7,11 +7,13 @@
                  [appengine "0.2"]
                  [com.google.appengine/appengine-api-1.0-sdk "1.3.4"]
                  [com.google.appengine/appengine-api-labs "1.3.4"]
-                 [fleet "0.9.5"]]
+                 [fleet "0.9.5"]
+		 [javax.mail/mail "1.4.3"]]
   :dev-dependencies [[swank-clojure "1.2.0"]
                      [ring/ring-jetty-adapter "0.2.0"]
                      [com.google.appengine/appengine-local-runtime "1.3.4"]
-                     [com.google.appengine/appengine-api-stubs "1.3.4"]]
+                     [com.google.appengine/appengine-api-stubs "1.3.4"]
+		     [javax.mail/mail "1.4.3"]]
   :repositories {"maven-gae-plugin" "http://maven-gae-plugin.googlecode.com/svn/repository"}
   :compile-path "war/WEB-INF/classes"
   :library-path "war/WEB-INF/lib")
